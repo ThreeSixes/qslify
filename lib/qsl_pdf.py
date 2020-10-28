7233198c4ee52ef513e9cee9bde5455ea4e9c4a2
ThreeSixes <joshlucy@gmail.com>
2020-10-28 12:40:17 -0700

    Add message, but it's not in the right position.

--- lib/qsl_pdf.py
+++ lib/qsl_pdf.py
@@ -29,6 +29,15 @@ class QslPdf:
         pass
 
 
+    def __add_address(self, address, size=8, location=(1.05 * inch, 9.35 * inch),
+                       color=colors.black):
+        """
+        Add the address to the canvas.
+        """
+
+        self.__draw_text(address, location, size, color=color, font="Helvetica")
+
+
     def __add_callsign(self, callsign, size=20, location=(1.05 * inch, 9.70 * inch),
                        color=colors.blue):
         """
@@ -38,13 +47,31 @@ class QslPdf:
         self.__draw_text(callsign, location, size, color=color, font="Helvetica-Bold")
 
 
-    def __add_address(self, address, size=12, location=(1.05 * inch, 9.5 * inch),
-                       color=colors.red):
+    def __add_frame(self, width=1, frame_color=colors.black, divider_color=colors.gray,
+                    message_color=colors.gray):
         """
-        Add the address to the canvas.
+        Add the frame and lines for the card as well as cut and fold instructions.
         """
 
-        self.__draw_text(tagline, location, size, color=color, font="Helvetica-Bold")
+        # Draw frame boxes
+        self.__canvas.setStrokeColor(frame_color)
+        self.__canvas.rect(1 * inch, 6.5 * inch, 5.5 * inch, 3.5 * inch, stroke=1, fill=0)
+        self.__canvas.rect(1 * inch, 3.0 * inch, 5.5 * inch, 3.5 * inch, stroke=1, fill=0)
+
+        # Top rectangle vertical divider
+        self.__canvas.setStrokeColor(divider_color)
+        divider_top = self.__canvas.beginPath()
+        divider_top.moveTo(3.75 * inch, 9.95 * inch)
+        divider_top.lineTo(3.75 * inch, 6.55 * inch)
+        self.__canvas.drawPath(divider_top, stroke=1, fill=0)
+
+        # Cut-and-fold instructions
+        self.__draw_text("(1) Cut around the outside border", (1 * inch, 2.85 * inch), 8,
+                         color=frame_color, font="Helvetica")
+        self.__draw_text("(2) Fold along this line", (6.55 * inch, 6.47 * inch), 8,
+                         color=frame_color, font="Helvetica")
+        self.__draw_text("Message", (7.55 * inch, 5.8 * inch), 20,
+                         color=message_color, font="Helvetica-Oblique")
 
 
     def __add_tagline(self, tagline, size=12, location=(1.05 * inch, 9.5 * inch),
@@ -70,14 +97,16 @@ class QslPdf:
         """
 
         canvas_config={'filename': filename}
-        location = (1 * inch, 9.5 * inch)
-
         self.__create_canvas(canvas_config)
 
+        # Draw card shapes
         self.__add_frame()
+        # Top to bottom, left to right
         self.__add_callsign(fields['callsign'])
         self.__add_tagline(fields['tagline'])
+        self.__add_address(fields['address'])
 
+        # Save our file.
         self.__canvas.save()
 
 
